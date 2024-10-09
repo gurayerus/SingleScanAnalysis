c9289e0fd84beeef2c50e9dbf16b754169abc586
gurayerus <gurayerus@gmail.com>
2024-10-09 17:41:50 -0400

    Add steps

---
+++ src/test_run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sid='Img1'
+simg='../test/input/Img1_T1.nii.gz'
+rid='Img2'
+rimg='../test/input/Img2_T1.nii.gz'
+out_dir='../test/output/Test1'
+
+python process_scan.py --sid $sid --simg $simg --rid $rid --rimg $rimg --out_dir $out_dir
+
+# >>>>>>>
+# ipython
+# run process_scan.py -m Sub1 -i ../test/input/Img1_T1.nii.gz  -o ../test/output/test1
